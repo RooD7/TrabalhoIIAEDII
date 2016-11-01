a49814d5da327b39972d36e1b9f2cb89ef393c71
RooD7 <rodrigosoualves7@gmail.com>
2016-11-01 13:59:05 -0200

    Pilha implementada

--- main.c
+++ main.c
@@ -2,8 +2,20 @@
 	Matricula:	0011264
 	Data:		27/10/2016
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha.h"
 
 int main(int argc, char const *argv[]) {
 	
+	stackNodePtr pilha;
+    int aux;
+
+    pilha = Pcria();
+    Pinsere(pilha, 6);
+    aux = Pexamina(pilha);
+    aux = Ptamanho(pilha);
+    aux = Premove(pilha);
+    Pdestroi(pilha);
 	return 0;
 }
