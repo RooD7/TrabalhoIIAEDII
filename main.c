c963a0b36e8723b10506f05b73a07b4dcb83c9d5
RooD7 <rodrigosoualves7@gmail.com>
2016-11-12 20:32:21 -0200

    Modularizado

--- main.c
+++ main.c
@@ -5,22 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pilha.h"
-#include "entrada.h"
+#include "entradaSaida.h"
 
 int main(int argc, char const *argv[]) {
 	
 	Pilha p;
     p = Pcria();
 
-	int op;
-	printf("Entrada de dados:\n");
-	printf("1. Via Tela.\n");
-	printf("2. Via Arquivo.\n");
-	scanf("%d", &op);
-	EntradaDeDados(p, op);
+	ESentradaDeDados(p);
 
     Pdestroi(p);
-	/*	a = Alibera(a); */
 
 	return 0;
 }
