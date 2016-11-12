a54220613d8c14946f3edc9cf2891623eec8f326
RooD7 <rodrigosoualves7@gmail.com>
2016-11-12 20:29:01 -0200

    Nome arquivo alterado, novo cabeçalho adicionado

---
+++ entradaSaida.h
@@ -0,0 +1,12 @@
+/*	Nome:		Rodrigo Sousa Alves
+	Matricula:	0011264
+	Data:		27/10/2016
+*/
+#include "arvore.h"
+#include "pilha.h"
+
+void ESentradaDeDados(Pilha p);
+void ESleituraArquivo(Pilha p);
+void ESleituraTela(Pilha p);
+void ESimprimeExpressao(Pilha p);
+void ESformataExpressao(NohArv arv);
